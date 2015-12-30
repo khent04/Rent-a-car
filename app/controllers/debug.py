b6c8f081dec82b4c990c97c5e5eecfbe37113e0a
khent <kenneth.estrella@cloudsherpas.com>
2015-12-30 19:56:40 +0800

    rename admin model to approver

---
+++ app/controllers/debug.py
@@ -0,0 +1,19 @@
+from ferris import route_with, BasicModel, messages
+from app.models.user.approver import Approver
+from app.controllers.base_controller import CarERentalController
+from ferris import Controller
+
+
+class Debug(CarERentalController):
+    class Meta:
+        prefixes = ("api",)
+        components = (messages.Messaging,)
+        Model = BasicModel
+
+    @route_with("/api/debug/create_admin/<email>")
+    def api_init_cet_users(self, email):
+        self.init_cet_users(email)
+        return 200
+
+    def init_cet_users(self, email):
+        Approver.create(email=email)
---
+++ app/models/user/approver.py
@@ -0,0 +1,6 @@
+from google.appengine.ext import ndb
+from app.models.user.user import User
+
+
+class Approver(User):
+    pass
